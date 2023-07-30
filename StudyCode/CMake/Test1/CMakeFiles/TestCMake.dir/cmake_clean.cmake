e5cf30140d91067d08394162472e510747cf7952
zy <1767703633@qq.com>
2023-07-30 21:17:34 +0800

    学习CMake

---
+++ StudyCode/CMake/Test1/CMakeFiles/TestCMake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/TestCMake.dir/home/zy/Server-Framework/StudyCode/CMake/TestCMake.cc.o"
+  "TestCMake.pdb"
+  "TestCMake"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/TestCMake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ StudyCode/readme.md
@@ -0,0 +1,3 @@
+这个目录用来存放开发过程中学习相关知识的配套代码
+# 1、CMake
+<a href="https://blog.csdn.net/whahu1989/article/details/82078563">Linux下CMake教程</a>
